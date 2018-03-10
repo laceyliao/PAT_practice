759bf3915411edab111022b4c198cd156fcafb0b
laceyliao <liaoyx1996@sina.com>
2018-03-10 14:17:22 +0800

    Rename A+Bpoly.cpp to 1002.A+Bpoly.cpp

--- 1002.A+Bpoly.cpp
+++ 1002.A+Bpoly.cpp
@@ -7,7 +7,7 @@ Input
 Each input file contains one test case. Each case occupies 2 lines, and each 
 line contains the information of a polynomial: K N1 aN1 N2 aN2 ... NK aNK, 
 where K is the number of nonzero terms in the polynomial, Ni and aNi (i=1, 2, ..., K) 
-are the exponents and coefficients, respectively. It is given that 1 <= K <= 10£¬0 <= NK < ... < N2 < N1 <=1000. 
+are the exponents and coefficients, respectively. It is given that 1 <= K <= 10Â£Â¬0 <= NK < ... < N2 < N1 <=1000. 
 
 Output
 For each test case you should output the sum of A and B in one line, with the same format as the input. 
